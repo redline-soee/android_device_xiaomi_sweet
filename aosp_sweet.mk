45ab6006c20712c5645a51ff2ea42528d7e0a9a8
redline-soee <25106499+redline-soee@users.noreply.github.com>
2022-05-28 14:11:24 +0200

    sweet: Init AEX

--- aosp_sweet.mk
+++ aosp_sweet.mk
@@ -7,14 +7,20 @@
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit common AEX configurations
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_sweet
+PRODUCT_NAME := aosp_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+WITH_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
